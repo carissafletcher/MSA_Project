efaf132f0252679824d53e8bad96e08066b2290c
Carissa <Carissafletcherregez@gmail.com>
2021-02-18 13:54:31 +0100

    1. Revised file after running and debugging.  Seems to be a problem with line12 #importing sentry_sdk (I am not sure if this is relevant)

--- MSA_v1.py
+++ MSA_v1.py
@@ -1,19 +1,19 @@
 from Bio import Phylo
 
-MSA Tool
+#MSA Tool
 
 #Start of the Sprint_3 project
 
-import  bio # biopython provides modules and scripts for python based bioinformaticians
+import  Bio # biopython provides modules and scripts for python based bioinformaticians
 import  datetime # a module that allows classes to manipulate dates and times
-import  OS # a module that interacts with an operating system to create folders
+import  os # a module that interacts with an operating system to create folders
 import  time # a module that provides time related functions
 import  xml.etree.ElementTree # A module that has an API for parsing and creating XML data
-import sentry_sdk # Enables automatic reporting of errors and exceptions
+#import sentry_sdk # Enables automatic reporting of errors and exceptions
 
-From Bio Import Entrez, BioSeqIO, BioAlignIO, Phylo # Entrez provides code to access the NCBI over the www, BioSeqIO standard sequence input/output interface for biopython (only dealing in RefSeq objects), BioAlign works directly with sequence alignment files as alignment objects, phylo provides classes and functions to support phylogenetic trees
-From Bio.Blast Import NCBIWWW # Modules enables www version of NCBI BLAST to be accessed
-From Bio.Cluster Import Node, Tree, treecluster # Nodes describes a single node in a hierarchical clustering tree, Tree performs a hierarchical clustering tree consisting of nodes and tree cluster also creates a hierarchical clustering tree but returns a tree object
+from Bio import Seq, Entrez, SeqIO, AlignIO, Phylo # Entrez provides code to access the NCBI over the www, BioSeqIO standard sequence input/output interface for biopython (only dealing in RefSeq objects), BioAlign works directly with sequence alignment files as alignment objects, phylo provides classes and functions to support phylogenetic trees
+from Bio.Blast import NCBIWWW # Modules enables www version of NCBI BLAST to be accessed
+from Bio.Cluster import Node, Tree, treecluster # Nodes describes a single node in a hierarchical clustering tree, Tree performs a hierarchical clustering tree consisting of nodes and tree cluster also creates a hierarchical clustering tree but returns a tree object
 
 
 def get_query_name():
